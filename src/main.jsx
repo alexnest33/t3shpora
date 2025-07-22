01eee58f150abf9bc996ff0e8d49c793239c253e
Aliaksandr nest <aliaksandrnest@Air-Aliaksandr.bbrouter>
2025-07-22 13:11:49 +0300

    updating

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router";
+import { RouterProvider} from "react-router/dom";
+import { createBrowserRouter } from "react-router";
 import "./index.css";
 import App from "./App.jsx";
 import routers from "./components/GetTopics/index.jsx";
