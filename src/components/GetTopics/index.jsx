f1869bf90abeb2953cf9631b70745f4bd1c06a46
Aliaksandr nest <aliaksandrnest@Air-Aliaksandr.bbrouter>
2025-07-22 10:13:23 +0300

    newUpdate

--- src/components/GetTopics/index.jsx
+++ src/components/GetTopics/index.jsx
@@ -1,15 +1,21 @@
 import { createBrowserRouter } from "react-router";
 import navigation from "../Navigation";
+import SwitchFunc from "./SwitchFunc"
 
 
 
-const routers = createBrowserRouter[
-    {path: "/",element: <App/>,children:navigation.map(items => {
-        const {path,title,content} = items;
-        return{
-            path:path,
-            element: 
-        }
-    })},
+    const routers = createBrowserRouter[
+        {path: "/",element: <App/>,children:navigation.map(items => {
+            const {path,title,content} = items;
+            return{
+                path:path,
+                element: <SwitchFunc title={title} content={content}/>
+            }
+        })}]
 
-]
+    export default routers
+
+
+
+
+
